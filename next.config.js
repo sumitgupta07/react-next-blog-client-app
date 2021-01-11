a3143dda8777cb517176032464ce953db3f2b5f9
sumitgupta07 <sumit.pec@gmail.com>
2021-01-10 19:50:25 -0500

    updated config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    APP_NAME: 'SEOBLOG',
+    API_DEVELOPMENT: 'http://localhost:8000/api',
+    API_PRODUCTION: 'http://localhost:8000/api',
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+    DOMAIN_PRODUCTION: 'https://tanechysolutions.com/seoblog',
+    FB_APP_ID: '1379762749026303',
+    DISQUS_SHORTNAME: 'miniblog-app',
+    GOOGLE_CLIENT_ID: '820003617956-o9kuece1n3f06fgtmomat30a4qbhqcut.apps.googleusercontent.com',
+  },
+});
